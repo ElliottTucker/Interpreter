d2e3da1e62f6163c0a8429c708dd50b0a7113f2d
Ellio <Ellio@PC2>
2026-05-12 06:58:24 +0100

    Add project files.

---
+++ Interpreter/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interpreter
+{
+    internal class Program
+    {
+        StreamReader InputFile = new StreamReader("InputFile.txt");
+    }
+}
---
+++ Interpreter/Tokens.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+namespace Interpreter
+{
+    public enum TokenType
+    {
+        //Values
+        Number,
+        String,
+        Bool,
+        Identifier,
+
+        //Keys
+        Let,
+        If,
+        Else,
+        Print,
+        While,
+        True,
+        False,
+
+        //Arithmatic
+        Plus,
+        Minus,
+        Star,
+        Slash,
+
+        //Symbols
+        Assign, //=
+        LeftBrace, RightBrace, //{ }
+        LeftParen, RightParen, //( )
+
+        //Comparison 
+        Equals, //==
+        NotEquals,
+        Less,
+        Greater,
+        LessEq,
+        GreaterEq,
+            
+        //End of file
+        EOF 
+    }
+
+    public class Token
+    {
+        public TokenType TypeOfToken;
+        public string TextInput;
+        public Token(TokenType tokenType,string textInput)
+        {
+            TypeOfToken = tokenType;
+            TextInput = textInput;
+        }
+
+        public override string ToString()
+        {
+            return ($"[{TypeOfToken} : {TextInput}]");
+        }
+    }
+}
